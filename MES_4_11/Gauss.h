0eb6d7d5c510870b12cd5adda471648f64f78765
Adam <72575016+adam-glab@users.noreply.github.com>
2021-11-04 22:09:34 +0100

    Fix gauss precision, cut down on console prints

---
+++ MES_4_11/Gauss.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <math.h>
+
+struct gauss {
+
+	// gauss x const
+	double* xC;
+
+	// weight points
+	double* wP;
+
+	int N;
+	gauss(int N0) : N(N0) {
+		if (N == 2) {
+			xC = new double[N];
+			xC[0] = -sqrt(1.0 / 3.0);
+			xC[1] = sqrt(1.0 / 3.0);
+
+			wP = new double[N];
+			wP[0] = 1.0;
+			wP[1] = 1.0;
+		}
+		else if (N == 3) {
+			xC = new double[N];
+			xC[0] = -0.774597;
+			xC[1] = 0.0;
+			xC[2] = 0.774597;
+
+			wP = new double[N];
+			wP[0] = 0.555555;
+			wP[1] = 0.888888;
+			wP[2] = 0.555555;
+		}
+		else {
+			// further wP,xC
+		}
+	}
+
+	~gauss() {
+		std::cout << "Gauss - destructor...\n";
+		delete[] xC;
+		delete[] wP;
+	}
+};
